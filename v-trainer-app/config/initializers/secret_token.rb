20939239679bbb85c7ca3cea47e1d54ab5be0650
Tino Amodei <t.amodei0@gmail.com>
2012-03-12 21:27:52 -0400

    Uploaded Rails App

---
+++ v-trainer-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VTrainerApp::Application.config.secret_token = '9f88422b026fd1fb6ba67b69593a509d91bfedf0af56b7cce1c2c4eae1b367896024fd789cc79a71834d28b83f8bd8ad0fbe4fce5e136f8de32dc603c18c3851'
